89b84249cf81e1935c11ff7e78d4051377440f4d
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-05 10:31:08 -0800

    Add rollDie function

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,7 @@ class Die extends Component {
   render() {
     return (
         <div className="Die">
-          <i className="fas fa-dice-one fa-10x"></i>
+          <i className={`fas fa-10x fa-dice-${this.props.num}`}></i>
         </div>
     );
   }
--- src/RollDice.js
+++ src/RollDice.js
@@ -5,17 +5,40 @@ import Die from "./Die";
 class RollDice extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      die1: 'one',
+      die2: 'one'
+    };
+    this.rollDie = this.rollDie.bind(this);
   }
+  rollDie = e => {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      const numWords = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+      };
+      const newRand = Math.floor(Math.random() * (max - min + 1)) + min;
+      return numWords[newRand];
+    }
+    const roll1 = getRandomInt(1, 6);
+    const roll2 = getRandomInt(1, 6);
+    this.setState({die1: roll1, die2: roll2})
+  };
 
   render() {
     return (
         <div className="RollDice">
           <div className="RollDice-die">
-            <Die />
-            <Die />
+            <Die num={this.state.die1}/>
+            <Die num={this.state.die2}/>
           </div>
-          <button className="RollDice-button">Rolling</button>
+          <button className="RollDice-button" onClick={this.rollDie}>Roll die</button>
         </div>
     );
   }
